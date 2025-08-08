c7ef3ab76ff20dce2c6a7ebc37670bd0844a6106
Daniel Schembri <dschem.ds@gmail.com>
2025-08-08 19:55:18 +0000

    Updated main.py

--- main.py
+++ main.py
@@ -4,35 +4,40 @@ import requests, arrow
 url = "https://calendar.google.com/calendar/ical/rneventteknik%40gmail.com/public/basic.ics"
 c = Calendar(requests.get(url).text)
 
-my_events = Calendar()
+def cal_generator(calendar: Calendar, full_name: str, initals: str):
+    my_events = Calendar()
 
-for e in list(c.timeline.start_after(arrow.now())):
-    name = e.name
-    
-    # if not a gig but contains Name
-    if "]" not in name:
-        if "daniel" in name.lower():
-            my_events.events.add(e)
-        continue
-
-    # if gig and contains name
-    names = []
-    for name in name.split("]")[0].strip().replace("[", "").split(","):
-        name = name.strip()
-        if len(name) == 2:
-            names.append(name)
+    for e in list(calendar.timeline.start_after(arrow.now())):
+        name = e.name
+        
+        # if not a gig but contains Name
+        if "]" not in name:
+            if full_name in name.lower():
+                my_events.events.add(e)
             continue
-        for name in name.split( ):
+
+        # if gig and contains name
+        names = []
+        for name in name.split("]")[0].strip().replace("[", "").split(","):
             name = name.strip()
             if len(name) == 2:
                 names.append(name)
                 continue
-    print(names)
+            for name in name.split( ):
+                name = name.strip()
+                if len(name) == 2:
+                    names.append(name)
+                    continue
 
-    for name in names:
-        if "DS" == name:
-            my_events.events.add(e)
+        for name in names:
+            if initals == name:
+                my_events.events.add(e)
+    
+    return my_events
 
 
-for e in list(my_events.timeline):
-    print("Event '{}' started {}".format(e.name, e.begin.humanize()))
+with open('Tessa.ics', 'w') as f:
+    f.writelines(cal_generator(c, "tessa", "TY").serialize_iter())
+
+with open('Daniel.ics', 'w') as f:
+    f.writelines(cal_generator(c, "daniel", "DS").serialize_iter())
